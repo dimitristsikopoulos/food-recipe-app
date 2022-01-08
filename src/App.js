072c13be9a22df53bc5064a019c3e979f84e9415
Dimitrios Tsikopoulos <dimitristsik@gmail.com>
2022-01-08 15:15:32 +0200

    Completed app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import Axios from "axios";
+import React, { useState } from "react";
+import "./App.css";
+import RecipeTile from "./components/RecipeTile";
 
-function App() {
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+  const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${process.env.REACT_APP_EDAMAM_APP_ID}&app_key=${process.env.REACT_APP_EDAMAM_APP_KEY}`;
+
+  async function getRecipes() {
+    const result = await Axios.get(url);
+    setRecipes(result.data.hits);
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    getRecipes();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Food Recipes Plaza</h1>
+      <form className="app__searchForm" onSubmit={onSubmit}>
+        <input
+          type="text"
+          className="app__input"
+          placeholder="enter ingridient"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <input type="submit" className="app__submit" value="Search" />
+      </form>
+      <div className="app__recipes">
+        {recipes.map((recipe) => {
+          return <RecipeTile recipe={recipe} />;
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
